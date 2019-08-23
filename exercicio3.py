c9c73ed76edb267490a8005cc79610f72f611d46
Mairton Matheus <38512161+mairtinhobb@users.noreply.github.com>
2019-08-23 19:23:37 -0300

    Delete exercicio3.py

--- exercicio3.py
+++
@@ -1,5 +0,0 @@
-num = (int)(input("DIGITE O PRIMEIRO VALOR"))
-num2 = (int)(input("DIGITE O SEGUNDO VALOR"))
-soma = num + num2
-#a concatenacao com + nao aceita diferentes tipos, apenas com virgula
-print("A SOMA EH:" , soma)
